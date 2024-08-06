02c8c1d5783742d3cbc634d8efd8b72606c141ac
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-08-06 03:14:18 -0300

    feat: swiperslide config on project

--- functions.php
+++ functions.php
@@ -10,6 +10,33 @@ function register_my_menus()
 }
 add_action('init', 'register_my_menus');
 
-require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+require_once get_template_directory() . '/in/class-wp-bootstrap-navwalker.php';
+function enqueue_swiper_slider()
+{
+    // Enfileirar o CSS do Swiper
+    wp_enqueue_style(
+        'swiper-css',
+        get_template_directory_uri() . '/in/swiper/swiper-bundle.min.css'
+    );
+
+    // Enfileirar o JS do Swiper
+    wp_enqueue_script(
+        'swiper-js',
+        get_template_directory_uri() .
+            '/in/swiper/swiper-element-bundle.min.js',
+        [],
+        null,
+        true
+    );
+
+    wp_enqueue_script(
+        'init-swiper-js',
+        get_template_directory_uri() . '/in/swiper/init.js',
+        ['swiper-js'],
+        null,
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'enqueue_swiper_slider');
 ?>
 
--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 
     <?php include 'header.php'; ?>
-    <?php if (is_front_page() || is_home()) {
-    } ?>
+    <?php if (is_front_page() || is_home()) { ?>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+     </div>
+    <?php } ?>
     <?php include 'footer.php'; ?>
