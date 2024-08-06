d9a70349bc592bbc1d0c13c842a993b76f7174e6
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-08-06 02:58:32 -0300

    feat: page container and title

--- page.php
+++ page.php
@@ -1,9 +1,17 @@
 <?php
 include 'header.php'; ?>
-<?php if (have_posts()) {
-    while (have_posts()) {
-        the_post();
-        the_content();
-    }
-} ?>
+<div class="container">
+<div class="row">
+    <div class="col">
+    <h1><?php the_title(); ?></h1>
+    <hr class="mt-2 mb-4" />
+    <?php if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            the_content();
+        }
+    } ?>
+    </div>
+</div>
+</div>
 <?php include 'footer.php'; ?>
