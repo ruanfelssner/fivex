c82753f064d74cfd2903d9f7db0ea68050aca58b
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-08-06 01:04:29 -0300

    refactor: :art: tailwind in scss

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php 
+// disabled the admin bar
+add_filter('show_admin_bar', '__return_false');
+
+?>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo get_bloginfo( 'name' ); ?> - <?php echo get_bloginfo( 'description' ); ?> | <?php wp_title(); ?></title>
     <?php wp_head(); ?>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 </head>
 <body <?php body_class(); ?>>
