8ff1308074c0002b35fb05a3cb1e344dc0f63a04
ruan.felssner <ruan.felssner@ntconsult.com.br>
2024-08-06 09:47:28 -0300

    feat: bg home color

--- index.php
+++ index.php
@@ -22,11 +22,10 @@
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
      </div>
-     <div class="container prose py-4">
+     <div class="container prose py-4 bg-black text-light">
         <div class="row">
             <div class="col">
                 <h1>A Five X Brasil</h1>
-                <hr />
                 <p>A 5 Five X Brasil é uma loja importadora especializada em rodas, pneus e acessórios automotivos, destacando-se no mercado por sua oferta de produtos de alta qualidade importados diretamente do Japão. Fundada por Robson Okamura, a empresa tem se consolidado como um nome de referência no setor, oferecendo aos seus clientes itens que combinam desempenho superior com design inovador.</p>
             </div>
         </div>
